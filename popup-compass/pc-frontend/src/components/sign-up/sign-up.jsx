2bf2997ac1b35c54285569544a229ad09c5c9721
ed-silva <32685663+ed-silva@users.noreply.github.com>
2018-04-25 22:48:11 -0700

    implementing sign-up form

--- popup-compass/pc-frontend/src/components/sign-up/sign-up.jsx
+++ popup-compass/pc-frontend/src/components/sign-up/sign-up.jsx
@@ -4,7 +4,16 @@ class SignUp extends Component {
   render() {
     return (
       <div>
-        <h1>Login</h1>
+        <h2 className="mb-5">Form register</h2>
+        <form>
+          <p className="h5 text-center mb-4">Sign up</p>
+          <Input label="Your name" icon="user" group type="email" validate error="wrong" success="right" />
+          <Input label="Your email" icon="envelope" group type="email" validate error="wrong" success="right" />
+          <Input label="Your password" icon="lock" group type="password" validate />
+          <div className="text-center">
+            <Button color="deep-orange">Sign up</Button>
+          </div>
+        </form>
 
       </div>
     );
