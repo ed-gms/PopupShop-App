efeee084e27fbc8188f04a7594cae1271ee0ec16
ed-silva <32685663+ed-silva@users.noreply.github.com>
2018-08-05 11:13:45 -0700

    adjusting footer

--- popup-compass/pc-frontend/src/components/footer/Footer.jsx
+++ popup-compass/pc-frontend/src/components/footer/Footer.jsx
@@ -1,30 +1,22 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class Footer extends Component {
-  render() {
-    return (
-      <footer className="page-footer elegant-color center-on-small-only fixed-bottom">
-        <div className="container">
-          <div className="row">
-
-            <div className="col-md-12">
-              <br/>
-              <br/>
-            </div>
-
+const Footer = () => {
+  return (
+    <footer className="page-footer elegant-color center-on-small-only fixed-bottom">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <br />
+            <br />
           </div>
         </div>
+      </div>
 
-        <div className="footer-copyright">
-          <div className="container-fluid text-center">
-            © 2017 Copyright
-          </div>
-        </div>
-
-      </footer>
-
-    );
-  }
-}
+      <div className="footer-copyright">
+        <div className="container-fluid text-center">© 2017 Copyright</div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
