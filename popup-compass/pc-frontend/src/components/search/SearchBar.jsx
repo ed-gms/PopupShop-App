44821373cefd137503b0880d9ef051bd265faa2f
ed-silva <32685663+ed-silva@users.noreply.github.com>
2018-08-10 15:20:28 -0700

    updating searchbar

--- popup-compass/pc-frontend/src/components/search/SearchBar.jsx
+++ popup-compass/pc-frontend/src/components/search/SearchBar.jsx
@@ -1,36 +1,35 @@
-import React, { Component } from 'react';
+import React from "react";
 
 const searchBarStyle = {
-  padding: 30,
-}
+  padding: 30
+};
 
-class SearchBar extends Component {
-
-  render() {
-    return (
-      <form className="form-inline active-cyan-4 d-flex justify-content-center"
-        style={searchBarStyle}
-        ref={self => { this.formInput = self }}
-        onSubmit={(e) => {
-          e.preventDefault()
-          if (this.formInput.search.value !== '') {
-            this.props.updateSearch(this.formInput.search.value)
-            this.formInput.search.value = ''
-          }
+const SearchBar = () => {
+  return (
+    <form
+      className="form-inline active-cyan-4 d-flex justify-content-center"
+      style={searchBarStyle}
+      ref={self => {
+        this.formInput = self;
+      }}
+      onSubmit={e => {
+        e.preventDefault();
+        if (this.formInput.search.value !== "") {
+          this.props.updateSearch(this.formInput.search.value);
+          this.formInput.search.value = "";
         }
-        }
-      >
-        <input
-          className="form-control form-control-lg mr-3 w-75" type="text"
-          placeholder="Search"
-          aria-label="Search"
-          name="search"
-        />
-        <i className="fa fa-search" aria-hidden="true"></i>
-      </form>
-
-    );
-  }
-}
+      }}
+    >
+      <input
+        className="form-control form-control-lg mr-3 w-75"
+        type="text"
+        placeholder="Search"
+        aria-label="Search"
+        name="search"
+      />
+      <i className="fa fa-search" aria-hidden="true" />
+    </form>
+  );
+};
 
 export default SearchBar;
